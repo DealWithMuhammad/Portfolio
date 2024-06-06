e373f14f9550ff20392c211d4bd82e98396f4aca
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-06-06 20:02:17 +0800

    feat: added resume

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -144,7 +144,7 @@ export default function Intro() {
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link> */}
         <a
-          href="/.pdf"
+          href="/MuhammadResume.pdf"
           download={true}
           className="group text-sm  px-4 py-2  bg-white sm:text-lg sm:px-7 sm:py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
         >
